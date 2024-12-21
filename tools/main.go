c06a01772db8c1caac860b36325ee16636de5431
Pranshu Srivastava <rexagod@gmail.com>
2024-12-21 10:32:22 +0530

    fixup! chore: add xBSD pipelines
    Signed-off-by: Pranshu Srivastava <rexagod@gmail.com>

--- tools/main.go
+++ tools/main.go
@@ -19,49 +19,39 @@ import (
 	"os"
 	"path/filepath"
 	"runtime"
-
-	"github.com/spf13/cobra"
 )
 
 func main() {
-	var rootCmd = &cobra.Command{}
-
-	var matchCmd = &cobra.Command{
-		Use:   "match [file]",
-		Short: "Check whether the file matches the context.",
-		Args:  cobra.ExactArgs(1),
-		Run: func(cmd *cobra.Command, args []string) {
-			// For debugging purposes, allow overriding these.
-			goos, found := os.LookupEnv("GOHOSTOS")
-			if !found {
-				goos = runtime.GOOS
-			}
-			goarch, found := os.LookupEnv("GOARCH")
-			if !found {
-				goarch = runtime.GOARCH
-			}
-			ctx := build.Context{
-				GOOS:   goos,
-				GOARCH: goarch,
-			}
-			abs, err := filepath.Abs(args[0])
-			if err != nil {
-				panic(err)
-			}
-			match, err := ctx.MatchFile(filepath.Dir(abs), filepath.Base(abs))
-			if err != nil {
-				fmt.Println("Error:", err)
-				os.Exit(1)
-			}
-			if match {
-				os.Exit(0)
-			}
-			os.Exit(1)
-		},
-	}
-
-	rootCmd.AddCommand(matchCmd)
-	if err := rootCmd.Execute(); err != nil {
+	if len(os.Args) != 2 {
+		fmt.Println(`
+Usage: match [file]`)
 		os.Exit(1)
 	}
+
+	// For debugging purposes, allow overriding these.
+	goos, found := os.LookupEnv("GOHOSTOS")
+	if !found {
+		goos = runtime.GOOS
+	}
+	goarch, found := os.LookupEnv("GOARCH")
+	if !found {
+		goarch = runtime.GOARCH
+	}
+	ctx := build.Context{
+		GOOS:   goos,
+		GOARCH: goarch,
+	}
+	abs, err := filepath.Abs(os.Args[1])
+	if err != nil {
+		panic(err)
+	}
+	match, err := ctx.MatchFile(filepath.Dir(abs), filepath.Base(abs))
+	if err != nil {
+		fmt.Println("Error:", err)
+		os.Exit(1)
+	}
+	if match {
+		os.Exit(0)
+	}
+	os.Exit(1)
 }
